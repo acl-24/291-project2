b1b642aeaab1bfc7146bdf6f91ba9aee0bb0fcfa
acl-24 <85856412+acl-24@users.noreply.github.com>
2022-11-15 19:39:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,22 @@
 
 import sys
 import pymongo
+import mainScreen as ms
+import time
+
+lj = __import__('load-json')
 
 if __name__ == "__main__":
-    #constructs the database and creates mainScreen
+    if len(sys.argv) == 1:
+        print("Please enter a valid database file!!")
+    else:
+        if len(sys.argv) > 2:
+            print("Error usage! Usage:main.py [json_name.json]")
+        else:
+            if sys.argv[1].endswith(".json"):
+                    port = int(input('please input a port number: '))
+                    db, dblp = lj.load_json(sys.argv[1], port)
+                    time.sleep(1)
+                    ms.mainScreen(db, dblp)
+            else:
+                print("The input file name is not a file name ending in '. json'")
