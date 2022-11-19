2ab946d40276e67e27e082ed2458e1b0539ef0e0
Ben8249 <85025800+Ben8249@users.noreply.github.com>
2022-11-19 11:41:56 -0700

    Update load-json.py

--- load-json.py
+++ load-json.py
@@ -4,13 +4,11 @@ def load_json(jsonfile, portno):
 
     jfile = open(jsonfile, 'r')
 
-    newdocs = []
-    for line in jfile:
-        newdocs.append(json.loads(line))
     portno = str(portno)
     url = 'mongodb://localhost:' + portno
     client = MongoClient(url)
     db = client["291_db"]
+  
 
     collist = db.list_collection_names()
     if 'dblp' in collist:
@@ -18,9 +16,21 @@ def load_json(jsonfile, portno):
     
     dblp = db['dblp']
 
-    dblp.insert_many(newdocs)
 
-    return db, dblp
+    for line in jfile:
+        dblp.insert_one(json.loads(line))
+
+    dblp.create_index([("year", 1)], name="index_year", unique=False)
+    dblp.create_index([("id", 1)], name="index_id", unique=True)
+    dblp.create_index([("title", 1)], name="index_title", unique=False)
+    dblp.create_index([("venue", 1)], name="index_venue", unique=False)
+    dblp.create_index([("references", 1)], name="index_ref", unique=False)
+    dblp.create_index([("n_citations", 1)], name="index_cit", unique=False)
+    dblp.create_index([("abstract", 1)], name="index_abstract", unique=False)
+    dblp.create_index([("authors", 1)], name="index_authors", unique=False)
+
+
+ 
 
 
 
