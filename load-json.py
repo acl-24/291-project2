756a5b774f665069fd1e449db827607083e584ee
acl-24 <a.liu24@outlook.com>
2022-11-19 17:50:39 -0700

    Added collcation for load_json

--- load-json.py
+++ load-json.py
@@ -1,5 +1,6 @@
 def load_json(jsonfile, portno):
     from pymongo import MongoClient
+    from pymongo.collation import Collation
     import json
 
     jfile = open(jsonfile, 'r')
@@ -21,17 +22,14 @@ def load_json(jsonfile, portno):
         dblp.insert_one(json.loads(line))
 
     dblp.create_index([("year", 1)], name="index_year", unique=False)
-    dblp.create_index([("id", 1)], name="index_id", unique=True)
-    dblp.create_index([("title", 1)], name="index_title", unique=False)
-    dblp.create_index([("venue", 1)], name="index_venue", unique=False)
-    dblp.create_index([("references", 1)], name="index_ref", unique=False)
-    dblp.create_index([("n_citation", 1)], name="index_cit", unique=False)
-    dblp.create_index([("abstract", 1)], name="index_abstract", unique=False)
-    dblp.create_index([("authors", 1)], name="index_authors", unique=False)
+    dblp.create_index([("id", 1)], name="index_id", unique=True, collation=Collation(locale= 'en', strength= 2))
+    dblp.create_index([("title", 1)], name="index_title", unique=False, collation=Collation(locale= 'en', strength= 2))
+    dblp.create_index([("venue", 1)], name="index_venue", unique=False, collation=Collation(locale= 'en', strength= 2))
+    dblp.create_index([("references", 1)], name="index_ref", unique=False, collation=Collation(locale= 'en', strength= 2))
+    dblp.create_index([("n_citation", 1)], name="index_cit", unique=False, collation=Collation(locale= 'en', strength= 2))
+    dblp.create_index([("abstract", 1)], name="index_abstract", unique=False, collation=Collation(locale= 'en', strength= 2))
+    dblp.create_index([("authors", 1)], name="index_authors", unique=False, collation=Collation(locale= 'en', strength= 2))
 
 
- 
-
-
-
+    return db, dblp
 
